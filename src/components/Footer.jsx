bde9fc741985f5d0a605c75e5751d968a9674070
clf94-dev <clf94@hotmail.es>
2020-09-05 18:34:31 +0200

    cta style structure added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,42 +2,44 @@ import React from 'react'
 import { Grid} from '@material-ui/core'
 import {Link} from 'react-router-dom';
 
+import Logo from '../images/shared/desktop/logo copy.svg'
+
 function Footer() {
     return (
         <div>
-            <Grid container direction='row'>
+            <Grid container direction='row' className='footer-cont'>
                 <Grid item direction='column' md={3} xs={12}>
-                    <h2>PHOTOSNAP</h2>
+                <img src={Logo} alt="logo"/> 
 
-                    <Grid direction='row'>
-                        <i className='fas fa-facebook'></i>
-                        <i className='fas fa-youtube'></i>
-                        <i className='fas fa-twitter'></i>
-                        <i className='fas fa-pinterest'></i>
-                        <i className='fas fa-instagram'></i>
+                    <Grid direction='row' className='social-row'>
+                        <i className='fab fa-facebook faceb fa-gradient'></i>
+                        <i className='fab fa-youtube yout'></i>
+                        <i className='fab fa-twitter twitt'></i>
+                        <i className='fab fa-pinterest pint'></i>
+                        <i className='fab fa-instagram instag'></i>
                     </Grid>
                 </Grid>
-                <Grid item direction='column' md={3} xs={12}>
+                <Grid item direction='column' md={3} xs={12} className='footer-links'>
                     <Link to='/'>
-                        <h3>Home</h3>
+                        <p>Home</p>
                     </Link>
                     <Link to='/stories'>
-                        <h3>Stories</h3>
+                        <p>Stories</p>
                     </Link>
                     <Link to='/features'>
-                        <h3>Features</h3>
+                        <p>Features</p>
                     </Link>
                     <Link to='/pricing'>
-                        <h3>Pricing</h3>
+                        <p>Pricing</p>
                     </Link>
 
                 </Grid>
                 <Grid item direction='column' md={3} xs={12}></Grid>
-                <Grid item direction='column' md={3} xs={12}>
+                <Grid item direction='column' md={3} xs={12} className='copy-col'>
                     <button>Get an Invite</button>
 
-                    <h5>
-                        Copyright 2019. All Rights Reserved</h5>
+                    <p>
+                        Copyright 2019. All Rights Reserved</p>
                 </Grid>
             </Grid>
         </div>
