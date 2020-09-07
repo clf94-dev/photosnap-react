ab0653a815d701a969d5f32df89f214e6887fcd8
clf94-dev <clf94@hotmail.es>
2020-09-07 10:26:43 +0200

    button component fixed style

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import {Grid} from '@material-ui/core'
 import CtaBGD from '../images/shared/desktop/bg-beta.jpg'
+
+import ArrowD from '../images/shared/desktop/arrow.svg'
+import ArrowL from '../images/shared/desktop/arrow copy.svg'
+
+import { Button } from './Button'
+import './styles/button.css'
+import zIndex from '@material-ui/core/styles/zIndex'
+
 function Cta() {
     return (
         <div>
@@ -13,7 +21,18 @@ function Cta() {
                             We're in Beta. Get your invite today!</h2>
                     </Grid>
                     <Grid item md={6} xs={12}>
-                        <button>Get An Invite</button>
+                        <Button buttonSize='btn--wide' buttonStyle='btn--read'>
+                            <Grid container direction='row'>
+                                <Grid item xs={8}>
+                                    GET AN INVITE
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img src={ArrowL} alt="arrow" />
+
+                                </Grid>
+                            </Grid>
+
+                        </Button>
                     </Grid>
                 </Grid>
             </Grid>
