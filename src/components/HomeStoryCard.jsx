ead60fa758e41035890faf34cc922e285e92f44f
clf94-dev <clf94@hotmail.es>
2020-09-13 12:01:00 +0200

    stories srcset added

--- src/components/HomeStoryCard.jsx
+++ src/components/HomeStoryCard.jsx
@@ -1,22 +1,6 @@
 import React from 'react'
-
-import Mountains from '../images/stories/desktop/mountains.jpg'
-import Sunset from '../images/stories/desktop/cityscapes.jpg'
-import Voyage from '../images/stories/desktop/18-days-voyage.jpg'
-import Architec from '../images/stories/desktop/architecturals.jpg'
-
-import World from '../images/stories/desktop/world-tour.jpg'
-import Corners from '../images/stories/desktop/unforeseen-corners.jpg'
-import King from '../images/stories/desktop/king-on-africa.jpg'
-import Nowhere from '../images/stories/desktop/trip-to-nowhere.jpg'
-import Rage from '../images/stories/desktop/rage-of-the-sea.jpg'
-import Free from '../images/stories/desktop/running-free.jpg'
-import Waves from '../images/stories/desktop/behind-the-waves.jpg'
-import Waters from '../images/stories/desktop/calm-waters.jpg'
-import Milky from '../images/stories/desktop/milky-way.jpg'
-import Forest from '../images/stories/desktop/dark-forest.jpg'
-import Beauty from '../images/stories/desktop/somwarpet.jpg'
-import Dreams from '../images/stories/desktop/land-of-dreams.jpg'
+import  {Mountains,Sunset, Voyage,Architec, World,Corners,King,Nowhere, Rage,Free, Waves,Waters, Milky,Forest,Beauty, Dreams} from './StoriesImg'
+import  { MountainsM, SunsetM, VoyageM, ArchitecM, WorldM, CornersM, KingM, NowhereM, RageM, FreeM, WavesM, WatersM, MilkyM, ForestM, BeautyM, DreamsM } from './StoriesImgMob'
 
 import ArrowD from '../images/shared/desktop/arrow.svg'
 import ArrowL from '../images/shared/desktop/arrow copy.svg'
@@ -48,6 +32,25 @@ function HomeStoryCard(props) {
         Beauty,
         Dreams
     ];
+    const HomeStoriesIMGM = [
+        MountainsM,
+        SunsetM,
+        VoyageM,
+        ArchitecM,
+        WorldM,
+        CornersM,
+        KingM,
+        NowhereM,
+        RageM,
+        FreeM,
+        WavesM,
+        WatersM,
+        MilkyM,
+        ForestM,
+        BeautyM,
+        DreamsM
+    ];
+  
     const Arrows = [ArrowL, ArrowD];
 
     return (
@@ -56,7 +59,7 @@ function HomeStoryCard(props) {
             <Grid container direction='row'>
 
                 <Grid item lg={12} className='img-cont'>
-                    <img src={HomeStoriesIMGD[props.index]} alt={props.alt}/>
+                    <img src={HomeStoriesIMGD[props.index]}  srcset={`${HomeStoriesIMGM[props.index]} 756w, ${HomeStoriesIMGD[props.index]} 1500w, ${HomeStoriesIMGD[props.index]} 1280w`} alt={props.alt}/>
                 </Grid>
                 <Grid item lg={12} className='text-col'>
                     <h4>{props.date}</h4>
