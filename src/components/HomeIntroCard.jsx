ba893f2ffa38888b6dfab5d200616a18cf1ae72b
clf94-dev <clf94@hotmail.es>
2020-09-13 10:36:13 +0200

    HomeIntro img srcset added

--- src/components/HomeIntroCard.jsx
+++ src/components/HomeIntroCard.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 import Create from '../images/home/desktop/create-and-share.jpg'
 import Stories from '../images/home/desktop/beautiful-stories.jpg'
 import Designed from '../images/home/desktop/designed-for-everyone.jpg'
+
+import CreateT from '../images/home/tablet/create-and-share.jpg'
+import StoriesT from '../images/home/tablet/beautiful-stories.jpg'
+import DesignedT from '../images/home/tablet/designed-for-everyone.jpg'
+
+import CreateM from '../images/home/mobile/create-and-share.jpg'
+import StoriesM from '../images/home/mobile/beautiful-stories.jpg'
+import DesignedM from '../images/home/mobile/designed-for-everyone.jpg'
+
+
 import {Grid} from '@material-ui/core'
 import {Button} from './Button'
 import './styles/button.css'
@@ -15,13 +25,15 @@ AOS.init();
 
 function HomeIntroCard(props) {
     const IntroIMGD = [Create, Stories, Designed]
+    const IntroIMGT = [CreateT, StoriesT, DesignedT]
+    const IntroIMGM = [CreateM, StoriesM, DesignedM]
     const Arrows=[ArrowL, ArrowD];
 
     return (
         <div>
             <Grid container direction='row' className={props.classes} data-aos='fade-up' data-aos-duration='1500'>
                 <Grid item lg={7} sm={5} xs={12} className='img-col'>
-                    <img src={IntroIMGD[props.index]} alt=""/>
+                    <img src={IntroIMGD[props.index]} srcset={`${IntroIMGM[props.index]} 756w, ${IntroIMGD[props.index]} 1500w, ${IntroIMGT[props.index]} 1280w`} alt=""/>
                 </Grid>
                 <Grid item lg={5} sm={7} xs={12} className='text-col'>
                     <h2>
